b70d41f7bda3a8b106eb75a76fb4dd726d771cfe
BruckyZ <bruckzewge1gmail.com>
2017-10-11 13:47:51 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+//Bruck Zewge , 10/10/2017, Java//
+package com.company;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+System.out.println("This is your name:" + "Bruck");
+        System.out.println("This is your age:" + 33);
+
+        }
+
+    }
+
